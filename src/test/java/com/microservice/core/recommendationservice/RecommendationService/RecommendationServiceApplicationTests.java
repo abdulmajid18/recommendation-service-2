40d8ed107c49ebd8722c9ae2241340ed1c236142
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-15 03:59:22 +0000

    initial commit

---
+++ src/test/java/com/microservice/core/recommendationservice/RecommendationService/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.core.recommendationservice.RecommendationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
